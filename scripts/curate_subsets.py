09b9b8c9a9ab88b72285f58f8e3478e5f1bd3401
Fscrew <cylcenlynn@gmail.com>
2024-12-15 13:10:03 -0500

    Update curate_subset to include OUTPUT_DIR from config file. Add encoding

--- scripts/curate_subsets.py
+++ scripts/curate_subsets.py
@@ -3,7 +3,10 @@ import random
 from util_filters import contain_numbers, contain_non_english, contain_unusual_proper_nouns
 
 DATA_PATH = "../data/JEOPARDY_QUESTIONS1.json"
-OUTPUT_DIR = "../data/"
+
+with open("config.json", "r", encoding="utf-8") as config_file:
+    config = json.load(config_file)
+OUTPUT_DIR = config["OUTPUT_DIR"]
 
 def filter_and_pick(data, filter_func, n=1000):
     """
@@ -24,20 +27,20 @@ def main():
     Output subsets to separate JSONL files.
     """
 
-    with open(DATA_PATH, "r") as file:
+    with open(DATA_PATH, "r", encoding="utf-8") as file:
         data = json.load(file)
 
     subset_numbers = filter_and_pick(data, contain_numbers, n=1000)
     subset_non_english = filter_and_pick(data, contain_non_english, n=1000)
     subset_unusual_proper_nouns = filter_and_pick(data, contain_unusual_proper_nouns, n=1000)
 
-    with open(f"{OUTPUT_DIR}subset_numbers.jsonl", "w") as file:
+    with open(f"{OUTPUT_DIR}subset_numbers.jsonl", "w", encoding="utf-8") as file:
         for obj in subset_numbers:
             file.write(json.dumps(obj) + '\n')
-    with open(f"{OUTPUT_DIR}subset_non_english.jsonl", "w") as file:
+    with open(f"{OUTPUT_DIR}subset_non_english.jsonl", "w", encoding="utf-8") as file:
         for obj in subset_non_english:
             file.write(json.dumps(obj) + '\n')
-    with open(f"{OUTPUT_DIR}subset_unusual_proper_nouns.jsonl", "w") as file:
+    with open(f"{OUTPUT_DIR}subset_unusual_proper_nouns.jsonl", "w", encoding="utf-8") as file:
         for obj in subset_unusual_proper_nouns:
             file.write(json.dumps(obj) + '\n')
 
